f91a0975cc0dcb4d1a7ba25f509a39c6ac53bb5b
Benjaminlal77 <benjaminlal2004@gmail.com>
2023-01-21 00:04:54 -0500

    Show hover and implement painting

--- scripts.js
+++ scripts.js
@@ -1,10 +1,23 @@
 let size=16;
 const canvas = document.getElementById('canvas');
+let boxes = [];
 for (let i=0; i<size; i++){
   for (let i2=0; i2<size; i2++){
     let box = document.createElement('div');
     box.classList.add('canvas-box');
+    box.draggable=false;
     box.style.width=`${(100/size)}%`;
+    boxes.push(box);
     canvas.appendChild(box);
   }
+}
+
+boxes.forEach(box=>{
+  box.addEventListener('mousedown', paintColor);
+  box.addEventListener('mouseenter', function(e){
+    if (e.buttons===1){paintColor(e);}
+  });
+});
+function paintColor(e){
+  e.target.style.backgroundColor='#000000';
 }
