a8cb3a337e547c00f502f6495aa8fd8830d83f74
Benjaminlal77 <benjaminlal2004@gmail.com>
2023-01-21 06:59:24 -0500

    Add grid size range

--- scripts.js
+++ scripts.js
@@ -1,44 +1,63 @@
 const canvas = document.getElementById('canvas');
 
-let size=16;
+let previousSize=0,size=16;
 let color='#000000';
-let boxes = [];
-for (let i=0; i<size; i++){
-  for (let i2=0; i2<size; i2++){
-    let box = document.createElement('div');
-    box.classList.add('canvas-box');
-    box.draggable=false;
-    box.style.width=`${(100/size)}%`;
-    boxes.push(box);
-    canvas.appendChild(box);
-  }
+changeGridSize();
+
+function addBox(){
+  let box = document.createElement('div');
+  box.classList.add('canvas-box');
+  box.draggable=false;
+  box.addEventListener('mousedown', paintColor);
+  box.addEventListener('mouseenter', function(e){
+    if (e.buttons===1){paintColor(e);}
+  });
+  canvas.appendChild(box);
+}
+function paintColor(e){
+  if (rainbowToggle.checked) color=getRandomColor();
+  e.target.style.backgroundColor=color;
 }
 
 const colorInput = document.getElementById('color-input');
 const penToggle = document.getElementById('pen-toggle');
 const eraserToggle = document.getElementById('eraser-toggle');
 const rainbowToggle = document.getElementById('rainbow-toggle');
+const gridSizeRange = document.getElementById('grid-size-range');
 const clearButton = document.getElementById('clear-button');
 
-colorInput.addEventListener('change', function(){if(penToggle.checked) color=colorInput.value;});
-penToggle.addEventListener('click', function(){color=colorInput.value;});
-eraserToggle.addEventListener('click', function(){color=null;});
-rainbowToggle.addEventListener('click', function(){color=getRandomColor();});
-clearButton.addEventListener('click', function(){boxes.forEach(box => {box.style.backgroundColor=null;});});
-
-function paintColor(e){
-  if (rainbowToggle.checked) color=getRandomColor();
-  e.target.style.backgroundColor=color;
-}
-boxes.forEach(box=>{
-  box.addEventListener('mousedown', paintColor);
-  box.addEventListener('mouseenter', function(e){
-    if (e.buttons===1){paintColor(e);}
-  });
+colorInput.addEventListener('change', ()=>{if(penToggle.checked) color=colorInput.value;});
+penToggle.addEventListener('click', ()=>{color=colorInput.value;});
+eraserToggle.addEventListener('click', ()=>{color=null;});
+rainbowToggle.addEventListener('click', ()=>{color=getRandomColor();});
+gridSizeRange.addEventListener('input', ()=>{
+  previousSize=size;
+  size=Number(gridSizeRange.value);
+  changeGridLabel();
+  changeGridSize();
 });
+clearButton.addEventListener('click', ()=>{
+  Array.from(canvas.children).forEach(box => {box.style.backgroundColor=null;});
+});
+
 function getRandomColor(){
-  let r=Math.floor((Math.random() * 255))+1;  
-  let g=Math.floor((Math.random() * 255))+1;
-  let b=Math.floor((Math.random() * 255))+1;  
+  const r=Math.floor((Math.random() * 255))+1;  
+  const g=Math.floor((Math.random() * 255))+1;
+  const b=Math.floor((Math.random() * 255))+1;  
   return `rgb(${r},${g},${b})`;
+}
+function changeGridLabel(){
+  const gridSizeLabel=Array.from(document.getElementsByClassName('grid-size-label'));
+  gridSizeLabel.forEach(label=>label.textContent=size);
+}
+function changeGridSize(){
+  const addingBoxes = canvas.childElementCount**.5<size; 
+  const boxesDifference = addingBoxes ? size**2-previousSize**2:previousSize**2-size**2;
+  
+  for (let i=0; i<boxesDifference;i++){
+    if (addingBoxes)addBox();
+    else canvas.removeChild(canvas.lastChild);
+  }
+  
+  Array.from(canvas.children).forEach(box=>{box.style.width = `${(100/size)}%`;});
 }
